06f65898bb179c30d95331a00a4ed888b71cfd42
Phillip Cloud <cpcloud@gmail.com>
2018-02-23 18:25:48 -0500

    License

--- setup.py
+++ setup.py
@@ -8,4 +8,7 @@ setup(
     packages=find_packages(),
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    description='Very concise API for writing Python ASTs',
+    license='Apache License, Version 2.0',
+    maintainer='Phillip Cloud',
 )
