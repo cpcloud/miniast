d8abd1a33a862859dcfbb85f50e0d32cee757dac
Phillip Cloud <cpcloud@gmail.com>
2018-02-24 15:04:27 -0600

    Author email

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='miniast',
+    url='https://github.com/cpcloud/miniast',
     packages=find_packages(),
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='Very concise API for writing Python ASTs',
     license='Apache License, Version 2.0',
-    maintainer='Phillip Cloud',
+    author='Phillip Cloud',
+    author_email='cpcloud@gmail.com',
 )
