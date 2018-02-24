21626218bd186cf3ea9b73fc3b2fc5166a2345e4
Phillip Cloud <cpcloud@gmail.com>
2018-02-24 12:20:45 -0600

    Cheat with in_

--- miniast/__init__.py
+++ miniast/__init__.py
@@ -1,5 +1,6 @@
 from .base import *  # noqa: F401, F403
-from .source import sourcify
+from .source import sourcify  # noqa: F401
+from toolz import identity as in_  # noqa: F401
 
 from ._version import get_versions
 __version__ = get_versions()['version']
