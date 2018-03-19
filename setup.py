6d58380ec3d529a83270e013e839c15bb3441632
Phillip Cloud <cpcloud@gmail.com>
2018-03-19 01:55:29 -0400

    Requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name='miniast',
     url='https://github.com/cpcloud/miniast',
     packages=find_packages(),
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='Lightweight macros for Python',
